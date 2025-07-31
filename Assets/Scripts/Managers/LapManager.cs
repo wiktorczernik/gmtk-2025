c89cc1f8d8f84e0d270afee015bacd62a34e4091
NykDevs <andreiutz240507@gmail.com>
2025-07-31 17:18:13 +0300

    Added Managers and stuff back

---
+++ Assets/Scripts/Managers/LapManager.cs
@@ -0,0 +1,59 @@
+using TMPro;
+using UnityEngine;
+
+public class LapManager : MonoBehaviour
+{
+    [SerializeField] GameObject LapsUI;
+    [SerializeField] private TextMeshProUGUI _timerText;
+
+    private int _currentLap = 1;
+    public int CurrentLap
+    {
+        get => _currentLap;
+        private set => _currentLap = value;
+    }
+
+    [SerializeField] private int TotalLaps = 3;
+
+    void Awake()
+    {
+        //GameManager.Instance.OnLapCompleted.AddListener(IncreaseLapCounter);
+        //GameManager.Instance.OnLapCompleted.AddListener(CheckForLapCompletion);
+        //GameManager.Instance.OnGameLoose.AddListener(TestGameLoose);
+    }
+
+    [ContextMenu("Increase Lap Counter")]
+    public void IncreaseLapCounter()
+    {
+        CurrentLap++;
+
+        if (CheckForLapCompletion())
+            return;
+
+        _timerText.text = $"{CurrentLap} / {TotalLaps}";
+    }
+
+    private bool CheckForLapCompletion()
+    {
+        if (CurrentLap > TotalLaps)
+        {
+            GameManager.Instance.AllLapsCompleted.Invoke();
+            return true;
+
+            //? Win The Game???
+        }
+
+        return false;
+    }
+
+
+    public void TestGameWin()
+    {
+        Debug.Log("Game Win");
+    }
+
+    public void TestGameLost()
+    {
+        Debug.Log("Game Lost");
+    }
+}

73f3e10090440a55564bf0b994525f84a4769e3f
omnicron-uno <Henryward732@gmail.com>
2025-08-03 11:41:36 +0930

    changed camera to be better again
    I didn't really change much

--- Assets/Scripts/FollowCarPoint.cs
+++ Assets/Scripts/FollowCarPoint.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class FollowCarPoint : MonoBehaviour
 {
     public Transform car;
-
     public void FixedUpdate()
     {
-        transform.position = car.position; // if someone can get this working, I would be very happy. Vector3.Lerp(transform.position, car.position, 0.5f);
+        // camera fly towards player like drone
+        transform.position = Vector3.Lerp(transform.position, car.position, 0.5f);
         Vector3 angles = car.eulerAngles;
         angles.x = 0;
         angles.z = 0;

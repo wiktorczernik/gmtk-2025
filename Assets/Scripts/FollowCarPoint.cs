6a544117a74035b60e485c17201b2383041f2459
omnicron-uno <Henryward732@gmail.com>
2025-08-03 10:38:34 +0930

    made reversing work
    when you reverse you turn now. but good though, and in the right direction.

--- Assets/Scripts/FollowCarPoint.cs
+++ Assets/Scripts/FollowCarPoint.cs
@@ -6,7 +6,7 @@ public class FollowCarPoint : MonoBehaviour
 
     public void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, car.position, 0.5f);
+        transform.position = car.position; // if someone can get this working, I would be very happy. Vector3.Lerp(transform.position, car.position, 0.5f);
         Vector3 angles = car.eulerAngles;
         angles.x = 0;
         angles.z = 0;
